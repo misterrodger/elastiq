7b239c9ee8c6f7917dc0404625dd1ddb4a0c71cb
John Rodger <john.rodger@atheneum.ai>
2026-01-21 00:38:41 +0000

    refactor bool-builder

--- src/bool-builder.ts
+++ src/bool-builder.ts
@@ -4,128 +4,97 @@ import { createQueryBuilder } from './query-builder';
 
 export const createBoolBuilder = <T>(
   state: QueryState = {}
-): BoolBuilder<T> => {
-  const ensureBool = (s: QueryState) => ({
-    ...s,
-    query: { bool: { ...(s.query?.bool || {}) } }
-  });
+): BoolBuilder<T> => ({
+  must: (fn) => {
+    const innerBuilder = fn(createQueryBuilder<T>({}));
+    const innerState = innerBuilder.build();
 
-  return {
-    must: (fn) => {
-      const innerBuilder = fn(createQueryBuilder<T>({}));
-      const innerState = innerBuilder.build();
+    return createBoolBuilder<T>(
+      Monad<T>(state)
+        .map((s) => ({
+          ...s,
+          query: {
+            bool: {
+              ...s.query?.bool,
+              must: [...(s.query?.bool.must || []), innerState.query]
+            }
+          }
+        }))
+        .fold()
+    );
+  },
 
-      return createBoolBuilder<T>(
-        Monad<T>(state)
-          .map((s) => {
-            const boolState = ensureBool(s);
-            return {
-              ...boolState,
-              query: {
-                bool: {
-                  ...boolState.query.bool,
-                  must: [...(boolState.query.bool.must || []), innerState.query]
-                }
-              }
-            };
-          })
-          .fold()
-      );
-    },
+  should: (fn) => {
+    const innerBuilder = fn(createQueryBuilder<T>({}));
+    const innerState = innerBuilder.build();
 
-    should: (fn) => {
-      const innerBuilder = fn(createQueryBuilder<T>({}));
-      const innerState = innerBuilder.build();
+    return createBoolBuilder<T>(
+      Monad<T>(state)
+        .map((s) => ({
+          ...s,
+          query: {
+            bool: {
+              ...s.query?.bool,
+              should: [...(s.query?.bool.should || []), innerState.query]
+            }
+          }
+        }))
+        .fold()
+    );
+  },
 
-      return createBoolBuilder<T>(
-        Monad<T>(state)
-          .map((s) => {
-            const boolState = ensureBool(s);
-            return {
-              ...boolState,
-              query: {
-                bool: {
-                  ...boolState.query.bool,
-                  should: [
-                    ...(boolState.query.bool.should || []),
-                    innerState.query
-                  ]
-                }
-              }
-            };
-          })
-          .fold()
-      );
-    },
+  mustNot: (fn) => {
+    const innerBuilder = fn(createQueryBuilder<T>({}));
+    const innerState = innerBuilder.build();
 
-    mustNot: (fn) => {
-      const innerBuilder = fn(createQueryBuilder<T>({}));
-      const innerState = innerBuilder.build();
+    return createBoolBuilder<T>(
+      Monad<T>(state)
+        .map((s) => ({
+          ...s,
+          query: {
+            bool: {
+              ...s.query?.bool,
+              must_not: [...(s.query?.bool.must_not || []), innerState.query]
+            }
+          }
+        }))
+        .fold()
+    );
+  },
 
-      return createBoolBuilder<T>(
-        Monad<T>(state)
-          .map((s) => {
-            const boolState = ensureBool(s);
-            return {
-              ...boolState,
-              query: {
-                bool: {
-                  ...boolState.query.bool,
-                  must_not: [
-                    ...(boolState.query.bool.must_not || []),
-                    innerState.query
-                  ]
-                }
-              }
-            };
-          })
-          .fold()
-      );
-    },
+  filter: (fn) => {
+    const innerBuilder = fn(createQueryBuilder<T>({}));
+    const innerState = innerBuilder.build();
 
-    filter: (fn) => {
-      const innerBuilder = fn(createQueryBuilder<T>({}));
-      const innerState = innerBuilder.build();
+    return createBoolBuilder<T>(
+      Monad<T>(state)
+        .map((s) => ({
+          ...s,
+          query: {
+            bool: {
+              ...s.query?.bool,
+              filter: [...(s.query?.bool.filter || []), innerState.query]
+            }
+          }
+        }))
+        .fold()
+    );
+  },
 
-      return createBoolBuilder<T>(
-        Monad<T>(state)
-          .map((s) => {
-            const boolState = ensureBool(s);
-            return {
-              ...boolState,
-              query: {
-                bool: {
-                  ...boolState.query.bool,
-                  filter: [
-                    ...(boolState.query.bool.filter || []),
-                    innerState.query
-                  ]
-                }
-              }
-            };
-          })
-          .fold()
-      );
-    },
+  minimumShouldMatch: (n) =>
+    createBoolBuilder<T>(
+      Monad<T>(state)
+        .map((s) => ({
+          ...s,
+          query: {
+            bool: {
+              ...s.query.bool,
+              minimum_should_match: n
+            }
+          }
+        }))
+        .fold()
+    ),
 
-    minimumShouldMatch: (n) =>
-      createBoolBuilder<T>(
-        Monad<T>(state)
-          .map((s) => {
-            const boolState = ensureBool(s);
-            return {
-              ...boolState,
-              query: {
-                bool: {
-                  ...boolState.query.bool,
-                  minimum_should_match: n
-                }
-              }
-            };
-          })
-          .fold()
-      ),
-
-    build: () => state
-  };
-};
+  build: () => state
+});
